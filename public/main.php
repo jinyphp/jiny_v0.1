bd077b7347106ab4f0a390e599d4dfb1fd68a0b8
hojinlee <infohojin@gmail.com>
2018-05-19 16:51:17 +0900

    composer init

--- public/main.php
+++ public/main.php
@@ -1,6 +1,7 @@
 <?php
 //오토로드 설정
-spl_autoload_register('spl_autoload', false);
+//spl_autoload_register('spl_autoload', false);
+require_once "../vendor/autoload.php";
     
 // 클래스 생성
 // Application의 객체를 생성합니다.
