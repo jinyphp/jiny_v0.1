3aa6646ddb5f0082a9f1adf4037bf8cc6240991c
hojinlee <infohojin@gmail.com>
2018-09-30 00:07:08 +0900

    edit index

--- app/conf/config.php
+++ app/conf/config.php
@@ -7,11 +7,15 @@
  * 사용자 정의 환경설정
  * 도메인, 포트 값을 통하여 환경설정값을 다르게 설정할 수 있습니다.
  */
+/*
 if ($_SERVER['SERVER_PORT'] == 8000) {
     $Config->setFile("site.ini","site");
 } else {
     $Config->setFile("site2.ini", "site");
 }
+*/
+
+$Config->setFile("site.ini","site");
 
 /**
  * 사용자 설정파일을 정의할 수 있습니다.
